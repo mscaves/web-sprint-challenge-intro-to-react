619ae3b23d9da7b6572ff017a3ab91b5d1329da6
Michael Caves <michael.scott.caves@gmail.com>
2021-05-02 18:40:53 -0400

    Setting up the project and charachter component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+
+//Need to import react
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Write a function that renders an individual character
+export default function Character(props){
+    const { currentCharacter, setCurrentCharacter } = props
+}
