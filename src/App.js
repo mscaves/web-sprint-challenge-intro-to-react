2e53a1a313d2ceca78f29158d328d100b8711207
Michael Caves <michael.scott.caves@gmail.com>
2021-05-02 19:46:19 -0400

    Pulling data from API via Axios and set up promise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import uuid from 'react-uuid'
+
+//Need to import API and get the character component from character.js
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +15,19 @@ const App = () => {
   // sync up with, if any.
 
   //I need to know the list of characters and a default character
-  const [ ]
+  const [characters, setCharacters ] = useState([])
+  const [activeCharacter, setActiveCharacter] = useState(null)
+
+  //Creating a side effect where we pull data from a different site
+  //Then creating a promise. Needs to resolve and error out. Could use the way we learned or async await
+  //After we get the data from the API we need to put it in an array
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people')
+    .then(resolve => {
+      setCharacters(resolve.data);
+    })
+    .catch(err => console.log(err));
+  }, [])
 
   return (
     <div className="App">
