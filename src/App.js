acda7b2e072b7ee7dedcbdf61edf4c530efed7bf
Michael Caves <michael.scott.caves@gmail.com>
2021-05-02 21:20:21 -0400

    Trying to send data from app.js to character.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
 
 //Need to import API and get the character component from character.js
 import axios from 'axios';
@@ -10,28 +9,28 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   //I need to know the list of characters and a default character
   const [characters, setCharacters ] = useState([])
-  const [activeCharacter, setActiveCharacter] = useState(null)
+  const [currentCharacter, setCurrentCharacter] = useState(null)
 
-  //Creating a side effect where we pull data from a different site
+  //Creating a effect hook where we pull data from a different site via API
   //Then creating a promise. Needs to resolve and error out. Could use the way we learned or async await
-  //After we get the data from the API we need to put it in an array
+  //Add a dependency array
   useEffect(() =>{
     axios.get('https://swapi.dev/api/people')
-    .then(resolve => {
-      setCharacters(resolve.data);
+    .then(res => {
+      setCharacters(res.data);
+      //setCharacters(res.data[0].name);
     })
     .catch(err => console.log(err));
   }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {/* <Character currentCharacter={currentCharacter}
+      setCurrentCharacter={setCurrentCharacter} /> */}
+
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,8 +3,17 @@
 //Need to import react
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from '../App';
 
 //Write a function that renders an individual character
-export function Character(props){
+export default function Character(props){
     const { currentCharacter, setCurrentCharacter } = props
 }
+
+// App.setCurrentCharacter.map(app => {
+//     return <Character name={App.name}
+// })
+
+//Render a single character to the DOM
+const rootElement = document.getElementById('root');
+//ReactDOM.render<Character/>, rootElement
